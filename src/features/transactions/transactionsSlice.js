194753d1d1d9bb9866dd69d3296ca583c5c338a9
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-04-21 17:45:20 +0700

    convert transactionsSlice using redux toolkit

--- src/features/transactions/transactionsSlice.js
+++ src/features/transactions/transactionsSlice.js
@@ -1,36 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
 export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
 const initialState = Object.fromEntries(CATEGORIES.map(category => [category, []]))
 
-export const addTransaction = (transaction) => {
-    return {
-        type: 'transactions/addTransaction',
-        payload: transaction
+//options for later to be used with createSlice
+const options = {
+    name: 'transactions',
+    initialState: initialState,
+    reducers: {
+        //payload is an object contain the transaction
+        // {
+        //     category: 'food',
+        //     description: 'groceries on 1/12/2021',
+        //     amount: 150,
+        //     id: 456
+        // }
+
+        //We will push the payload object into appropriate category
+        //use {} after => because we mutate the state already, without the {} the => will become implicit return and also return a different value which violate Redux return data rule
+        addTransaction: (transactionsState, action) => {
+            transactionsState[action.payload.category].push(action.payload);
+        },
+
+        //no need to use {} because filter doesn't mutate the state, so it is ok for us to implicit return a different value
+        deleteTransaction: (transactionState, action) => transactionState[action.payload.category].filter(transaction => transaction.id !== action.payload.id)
     }
 }
-
-export const deleteTransaction = (transaction) => {
-    return {
-        type: 'transactions/deleteTransaction',
-        payload: transaction
-    }
-}
-
+const transactionsSlice = createSlice(options);
+export default transactionsSlice.reducer;
+export const { addTransaction, deleteTransaction } = transactionsSlice.actions;
+//selectors
 export const selectTransactions = (state) => state.transactions;
 export const selectFlattenedTransactions = (state) => Object.values(state.transactions).reduce((a, b) => [...a, ...b], []);
-
-const transactionsReducer = (state = initialState, action) => {
-    let newTransactionsForCategory;
-    switch (action.type) {
-        case 'transactions/addTransaction':
-            newTransactionsForCategory = [...state[action.payload.category].slice(), action.payload]
-            return { ...state, [action.payload.category]: newTransactionsForCategory }
-        case 'transactions/deleteTransaction':
-            const deletedIndex = state[action.payload.category].findIndex(transaction => transaction.id === action.payload.id);
-            newTransactionsForCategory = state[action.payload.category].filter((item, index) => index !== deletedIndex)
-            return { ...state, [action.payload.category]: newTransactionsForCategory }
-        default:
-            return state;
-    }
-}
-
-export default transactionsReducer;
